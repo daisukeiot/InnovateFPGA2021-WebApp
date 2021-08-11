29594b5a6e65b6f13ec1ded4b2667556a42a8476
Daisuke Nakahara <daisuken@msn.com>
2021-08-11 16:26:24 -0700

    Initial Release

---
+++ InnovateFPGA2021-WebApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using InnovateFPGA2021_WebApp.Helper;
+using InnovateFPGA2021_WebApp.Models;
+
+namespace Portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppSettings _appSettings;
+        private HomeView _homeView;
+
+        public HomeController(IOptions<AppSettings> optionsAccessor, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _appSettings = optionsAccessor.Value;
+            _logger.LogInformation("HomeController");
+            _homeView = new HomeView();
+        }
+
+        public IActionResult Index()
+        {
+            HomeView homeView = _homeView;
+            return View(homeView);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ InnovateFPGA2021-WebApp/Models/HomeView.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace  InnovateFPGA2021_WebApp.Models
+{
+    public class HomeView
+    {
+        [DisplayName("Select Device")]
+        public string deviceId { get; set; }
+        public string moduleId { get; set; }
+        public IEnumerable<SelectListItem> deviceList { get; set; }
+        public IEnumerable<SelectListItem> moduleList { get; set; }
+        public string newDeviceId { get; set; }
+        public string registrationId { get; set; }
+        public DpsEnrollmentListViewModel dpsEnrollmentList { get; set; }
+        public DpsEnrollmentListViewModel dpsGroupEnrollmentList { get; set; }
+    }
+
+    public class IoTHubDeviceListViewModel
+    {
+        public IoTHubDeviceListViewModel()
+        {
+            Devices = new List<IoTHubDeviceViewModel>();
+        }
+
+        [Display(Name = "IoT Hub Device List")]
+        [Required]
+        public string SelectedIoTHubDevice { get; set; }
+        public IList<IoTHubDeviceViewModel> Devices { get; set; }
+    }
+
+    public class IoTHubDeviceViewModel
+    {
+        public string DeviceId { get; set; }
+        public string AuthenticationType {get;set;}
+        public bool IsEdge {get;set;}
+        public string ModelId {get;set;}
+        public string Status {get;set;}
+    }
+
+    public class DpsEnrollmentListViewModel
+    {
+        public DpsEnrollmentListViewModel()
+        {
+            Enrollments = new List<EnrollmentViewModel>();
+        }
+
+        [Display(Name = "DPS Enrollment List")]
+        [Required]
+        public string SelectedEnrollment { get; set; }
+        public IList<EnrollmentViewModel> Enrollments { get; set; }
+    }
+
+    public class DpsGroupEnrollmentListViewModel
+    {
+        public DpsGroupEnrollmentListViewModel()
+        {
+            Enrollments = new List<EnrollmentViewModel>();
+        }
+
+        [Display(Name = "DPS Group Enrollment List")]
+        [Required]
+        public string SelectedEnrollment { get; set; }
+        public IList<EnrollmentViewModel> Enrollments { get; set; }
+    }
+
+    public class EnrollmentViewModel
+    {
+        public string RegistrationId { get; set; }
+        public bool isGroup { get; set; }
+    }
+}
