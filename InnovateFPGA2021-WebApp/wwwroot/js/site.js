094a8d5ed9d34901155f2960a85ac5ee9f4045cd
Daisuke Nakahara <daisuken@msn.com>
2021-08-11 22:10:51 -0700

    cleanup

--- InnovateFPGA2021-WebApp/wwwroot/js/site.js
+++ InnovateFPGA2021-WebApp/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleEvent(item) {
+
+    var expanded = $(item).closest('.accordian-toggle').attr('aria-expanded');
+
+    console.log("Expanded " + expanded);
+    if (expanded == 'false' || expanded == undefined)
+    {
+        $(item).removeClass('fa-angle-double-down');
+        $(item).addClass('fa-angle-double-up');
+    }
+    else {
+        $(item).removeClass('fa-angle-double-up');
+        $(item).addClass('fa-angle-double-down');
+    }
+}
